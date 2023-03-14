3db3b19c0788cab46bdc7e6c2d3a1cfb8b8c9cf2
Alessandro Thea <alessandro.thea@cern.ch>
2023-03-14 08:53:08 +0000

    crappyhal_srv ported to C++

--- scripts/crappyhal.py
+++ scripts/crappyhal.py
@@ -1,8 +1,7 @@
 import json
 import devmem
 import time
-import sys, subprocess
-# import sh
+import sys
 import devmem
 
 class CrappyRawHardware : 
@@ -21,33 +20,6 @@ class CrappyRawHardware :
         b = self.mem.read(a*4, n)
         return b.data
 
-    # def _wreg(self, a, d):
-    #     for v in d:
-    #         if self.VERBOSE: print("w", hex(v), "->", hex(a))
-    #         axiaddr = self.AXI_OFFSET + a * 4
-    #         try:
-    #             r = sh.sudo("devmem", hex(axiaddr), "32", str(v))
-    #         except sh.ErrorReturnCode as e:
-    #             print(f"Write error at {hex(a)} : {e.exit_code}")
-    #             sys.exit(e.exit_code)
-
-    #         a += 1
-
-    # def _rreg(self, a, n):
-    #     d = []
-    #     for i in range(n):
-    #         axiaddr = self.AXI_OFFSET + a * 4
-    #         try:
-    #             r = sh.sudo("devmem", hex(axiaddr), "32")
-    #         except sh.ErrorReturnCode as e:
-    #             print(f"Read error at {hex(a)} : {e.exit_code}")
-    #             sys.exit(e.exit_code)
-
-    #         v = int(r.rstrip(), 16)
-    #         d.append(v)
-    #         if self.VERBOSE: print("r", hex(a), "==", hex(v))
-    #         a += 1
-    #     return d
 
     def get_shift_and_mask(self, mask):
         for s in range(32):
@@ -96,72 +68,7 @@ class CrappyHardware(CrappyRawHardware):
 
     @property
     def addrtab(self):
-        return self._addrtab
-
-
-    # @staticmethod
-    # def _wreg(a, d):
-    #     for v in d:
-    #         if CrappyHW.VERBOSE: print("w", hex(v), "->", hex(a))
-    #         axiaddr = CrappyHW.AXI_OFFSET + a * 4
-    #         try:
-    #             r = sh.sudo("devmem", hex(axiaddr), "32", str(v))
-    #         except sh.ErrorReturnCode as e:
-    #             print(f"Write error at {hex(a)} : {e.exit_code}")
-    #             sys.exit(e.exit_code)
-
-    #         a += 1
-
-    # @staticmethod
-    # def _rreg(a, n):
-    #     d = []
-    #     for i in range(n):
-    #         axiaddr = CrappyHW.AXI_OFFSET + a * 4
-    #         try:
-    #             r = sh.sudo("devmem", hex(axiaddr), "32")
-    #         except sh.ErrorReturnCode as e:
-    #             print(f"Read error at {hex(a)} : {e.exit_code}")
-    #             sys.exit(e.exit_code)
-
-    #         v = int(r.rstrip(), 16)
-    #         d.append(v)
-    #         if CrappyHW.VERBOSE: print("r", hex(a), "==", hex(v))
-    #         a += 1
-    #     return d
-
-    # def get_shift_and_mask(self, mask):
-    #     for s in range(32):
-    #         m = (mask >> s)
-    #         if m & 1:
-    #             break
-    #     return s, m
-
-
-    # def read_addr(self, addr, mask):
-    #     if self.VERBOSE: print(hex(addr), hex(mask))
-
-    #     val =  self._rreg(addr, 1)[0]
-
-    #     if mask == 0xffffffff:
-    #         return val
-    #     else:
-    #         s, m = self.get_shift_and_mask(mask)
-
-    #     return ((val & mask) >> s)
-
-
-    # def write_addr(self, addr, mask, val):
-    #     if mask == 0xffffffff:
-    #         self._wreg(addr, [val])
-    #     else:
-    #         # Read-modify-write
-    #         s, m = self.get_shift_and_mask(mask)
-
-    #         reg_val = self._rreg(addr, 1)[0]
-            
-    #         reg_new = (reg_val & ~mask) | ((val & m) << s)
-    #         self._wreg(addr, [reg_new]) 
-
+        return self._addrtabt
 
     def read(self, name):
         if not name in self._addrtab:
---
+++ src/crappyhal_srv.cxx
@@ -0,0 +1,214 @@
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <sys/mman.h>
+#include <set>
+#include <algorithm>
+#include <sstream>
+
+#include <zmq.hpp>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+// struct DevMemMappingError : public std::exception
+// {
+// 	const char * what () const throw ()
+//     {
+//     	return "Failed to map /dev/mem to memory";
+//     }
+// };
+
+uint32_t get_bitshift(uint32_t mask) {
+    uint32_t m = mask;
+    for( size_t s(0); s<32; ++s) {
+        m = mask >> s;
+        if ( m & 0x1 )
+            return s;
+    }
+
+    return 32;
+}
+
+class CrappyHardware {
+public:
+    CrappyHardware(size_t base_addr, size_t addr_len) {
+	    m_base_addr = base_addr;
+        m_addr_len = addr_len;
+
+        int r = mem_map();
+        std::cout << r << std::endl;
+    }
+
+    ~CrappyHardware() {
+        mem_unmap();
+    }
+
+
+
+    uint32_t read_addr(uint32_t addr, uint32_t mask) {
+        // if self.VERBOSE: print(hex(addr), hex(mask))
+
+        uint32_t val = *(m_base_ptr+addr);
+        if (mask == 0xffffffff)
+            return val;
+
+        uint32_t s = get_bitshift(mask);
+        return ((val & mask) >> s);
+    }
+
+    void write_addr(uint32_t addr, uint32_t mask, uint32_t val) {
+        if (mask == 0xffffffff) {
+            *(m_base_ptr+addr) = val;
+        } else {
+            uint32_t s = get_bitshift(mask);
+            uint32_t m = mask >> s;
+            uint32_t reg_val = *(m_base_ptr+addr);
+
+            *(m_base_ptr+addr) = (reg_val & ~mask) | ((val & m) << s);
+        }
+    }
+
+private:
+
+    int mem_map() {
+
+	    m_dev_mem_fd=::open("/dev/mem",O_RDWR|O_SYNC);
+	    if (m_dev_mem_fd < 1) 
+            return -1;
+            // throw DevMemMappingError();
+
+	    size_t ptr = (size_t) mmap(NULL,m_addr_len*4,PROT_READ|PROT_WRITE,MAP_SHARED,m_dev_mem_fd,m_base_addr);
+	    if (ptr == -1) 
+            return -2;
+            // throw DevMemMappingError();
+
+        m_base_ptr = (uint32_t*)(ptr);
+        return 0;
+    }
+
+    int mem_unmap() {
+        munmap(m_base_ptr,m_addr_len*4);
+        ::close(m_dev_mem_fd);
+        return 0;
+    }   
+
+    int m_dev_mem_fd;
+    size_t m_base_addr;
+    size_t m_addr_len;
+    uint32_t* m_base_ptr;
+
+};
+
+
+void json_reply(zmq::socket_t& socket, const json& data) {
+    std::string reply_str = data.dump(); 
+    // std::cout << "sending message '" << reply_str << "'" << std::endl;
+
+    zmq::message_t reply(reply_str.size());
+    memcpy((void*)reply.data(), reply_str.c_str(), reply_str.size());
+    socket.send(reply,zmq::send_flags::none);
+}
+
+json json_receive(zmq::socket_t& socket) {
+
+    zmq::message_t msg;
+    auto r = socket.recv(msg,zmq::recv_flags::none);        
+    std::string msg_str((char*)msg.data(), msg.size());
+    // std::cout << "received message '" << msg_str << "'" << std::endl;
+    return json::parse(msg_str);
+}
+
+int main(int argc, char* argv[]) {
+
+    // json data;
+    // data["xxx"] = 1;
+
+    // std::cout << "Hello World " << data << std::endl;
+    std::cout << "Hello World "<< std::endl;
+
+    CrappyHardware hw(0x80000000, 0x10000);
+    uint32_t v;
+    v = hw.read_addr(0x0, 0xffffffff);
+    // std::cout << std::hex << v << std::endl;
+
+    zmq::context_t context;
+    zmq::socket_t socket(context, ZMQ_REP);
+
+    socket.bind("tcp://*:5556");
+
+    for (int i = 0; ; i++) {
+
+        json data;
+        try {
+            data = json_receive(socket);
+
+        } catch ( std::exception &e ) {
+            std::cerr << "Failed to deserialize command message to json " << e.what() << std::endl;
+            json reply_data = { {"err", "InvalidJSONFormat"} };
+            json_reply(socket, reply_data);
+            continue;
+        }
+
+        std::set<std::string> keys;
+        for (auto& [key, val] : data.items())
+        {
+            // std::cout << "key: " << key << ", value:" << val << '\n';
+            keys.insert(key);
+        }
+        std::set<std::string> good_keys = {"cmd", "addr", "mask", "val"};
+        if (!std::includes(good_keys.begin(), good_keys.end(), keys.begin(), keys.end())) {
+            std::cerr << "Invalid message received" << std::endl;
+            json reply_data = { {"err", "InvalidMessage"} };
+            json_reply(socket, reply_data);
+            continue;
+        }
+
+        std::string cmd = data["cmd"];
+        long addr = std::stol (data["addr"].get<std::string>(),nullptr,16);
+        long mask = std::stol (data["mask"].get<std::string>(),nullptr,16);
+
+        // std::cout << "addr " << data["addr"] << " " << data["addr"].get<std::string>() << " " << addr << std::endl;
+        // std::cout << "mask " << data["mask"] << " " << data["mask"].get<std::string>() << " " << mask << std::endl;
+
+        if ( addr < 0 or addr > 0xffffffff ) {
+            std::cerr << "Invalid address received" << std::endl;
+            json_reply(socket, {{"err", "InvalidAddress"}});
+            continue;
+
+        }
+        if ( mask < 0 or mask > 0xffffffff ) {
+            std::cerr << "Invalid mask received" << std::endl;
+            json_reply(socket, {{"err", "InvalidMask"}});
+            continue;
+        }
+
+        if ( cmd == "read" ) {
+            uint32_t val = hw.read_addr(addr, mask);
+            std::cout << "Read 0x" << std::hex << val << " at 0x" << addr << " with mask 0x" << mask << std::endl;
+            std::stringstream ss;
+            ss << "0x" << std::hex << val;
+            json_reply(socket, {{"read_val", ss.str()}} );
+            continue;
+        } else if ( cmd == "write" ) {
+            long val = std::stol(data["val"].get<std::string>(),nullptr,32);
+            hw.write_addr(addr, mask, val);
+            std::cout << "Write 0x" << std::hex << val << " at 0x" << addr << " with mask ox" << mask << std::endl;
+            json_reply(socket, {{"write_done", true}} );
+            continue;
+
+        } else {
+            std::cerr << "Invalid command received" << std::endl;
+            json_reply(socket, {{"error", "InvalidCommand"}} );
+            continue;
+        }
+
+        json reply_data = {};
+        json_reply(socket, reply_data);
+
+    }
+    return 0;
+
+}
