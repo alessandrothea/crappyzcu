7cec9f8cce59abbface295803829ab078ec77a48
Alessandro Thea <alessandro.thea@cern.ch>
2023-03-28 05:24:48 +0000

    Bugfix

--- zynq/init_scripts/crappyhal_srv_wib.sh
+++ zynq/init_scripts/crappyhal_srv_wib.sh
@@ -1,2 +1,2 @@
 echo "Starting crappyhw_srv"
-/bin/crappyhw_srv wib 2>/var/log/crappyhw_server.err >/var/log/crappyhw_server.log &
+/bin/crappyhal_srv wib 2>/var/log/crappyhw_server.err >/var/log/crappyhw_server.log &
