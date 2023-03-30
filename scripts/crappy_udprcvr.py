b58952c30bf8f32b15e6c3cc881b3120ffaf103c
Alessandro Thea <alessandro.thea@cern.ch>
2023-03-30 08:27:31 +0000

    uhal udp-bridge prototype

--- scripts/crappy_udprcvr.py
+++ scripts/crappy_udprcvr.py
@@ -18,7 +18,7 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(2048) # buffer size is 1024 bytes
-    print(f"received message: {data} {addr}")
+    # print(f"received message: {data} {addr}")
 
     u32_data = [int.from_bytes(data[4*i:4*(i+1)], byteorder='little', signed=False) for i in range(len(data)//4)]
 
@@ -29,68 +29,69 @@ while True:
     next_req_page = status[2]
     num_replies = status[3]
 
-    print(f"Num buf       : {num_buf} {status[0]:08x}")
-    print(f"Word per page : {word_per_page} {status[1]:08x}")
-    print(f"Next page     : {next_req_page} {status[2]:08x}")
-    print(f"Num replies   : {num_replies} {status[3]:08x}")
+    # Print status info
+    # print(f"Num buf       : {num_buf}")
+    # print(f"Word per page : {word_per_page}")
+    # print(f"Next page     : {next_req_page}")
+    # print(f"Num replies   : {num_replies}")
 
-    next_req_base_addr = word_per_page*next_req_page
-    print(len(u32_data), [hex(d) for d in u32_data])
-    data_len = (IPB_MB_OFFSET | (len(u32_data)-1))
-    print(f"Writing pkt len word {data_len:08x} at addr {next_req_base_addr:08x} (page {next_req_page})")
-    mem.write(4*next_req_base_addr, [data_len])
+    # Next address page
+    next_req_base_addr = word_per_page * next_req_page
+    # print(len(u32_data), [hex(d) for d in u32_data])
+    data_len = (IPB_MB_OFFSET | (len(u32_data) - 1))
+    # print(f"Writing pkt len word {data_len:08x} at addr {next_req_base_addr:08x} (page {next_req_page})")
+
+    # Writing packet length in the first word of the buffer
+    mem.write(4 * next_req_base_addr, [data_len])
     
-    print(f"Writing pkt data at addr {next_req_base_addr+1:08x}")
-    mem.write(4*(next_req_base_addr+1), u32_data)
-    # for i,w in enumerate(u32_data):
-        # print(f"Writing {i} word {w:08x} at addr {next_req_base_addr+1+i:08x}")
-        # mem.write(4*(next_req_base_addr+1+i), [w])
+    # print(f"Writing pkt data at addr {next_req_base_addr+1:08x}")
+    # Writing packet into the buffer
+    mem.write( 4 * (next_req_base_addr + 1), u32_data)
 
-
-
-    reply_page = None
+    # Wait for reply to be available
     while True:
         b = mem.read(0, 4)
         new_num_replies = b[3]
         if new_num_replies != num_replies:
-            print(f"Old replies: {num_replies}, new replies {new_num_replies}")
-            reply_page = new_num_replies
+            # print(f"Old replies: {num_replies}, new replies {new_num_replies}")
             break
 
-    for p in range(num_buf):
-        paddr = 4+word_per_page*p
-        print(f"  DEBUG: reading reply size at addr {paddr:08x} (page {p})")
-        r = mem.read(4*paddr, 16)
-        for w in r:
-            print(f"{    w:08x}")
-        rs = r[0] & ~IPB_MB_OFFSET
-        print(f"  DEBUG: Reply size {rs} (reg {r[0]:08x})")
+    # Debug info
+    # for p in range(num_buf):
+    #     paddr = 4+word_per_page*p
+    #     print(f"  DEBUG: reading reply size at addr {paddr:08x} (page {p})")
+    #     r = mem.read(4*paddr, 16)
+    #     for w in r:
+    #         print(f"{    w:08x}")
+    #     rs = r[0] & ~IPB_MB_OFFSET
+    #     print(f"  DEBUG: Reply size {rs} (reg {r[0]:08x})")
 
 
-    next_rep_base_addr = 4+word_per_page*next_req_page
-    print(f"Reading reply size at addr {next_rep_base_addr:08x} (page {next_req_page})")
-    repl_size_reg = mem.read(4*next_rep_base_addr, 1)
-    repl_size = (repl_size_reg[0] & ~IPB_MB_OFFSET)+1
-    print(f"Reply size {repl_size} (reg {repl_size_reg[0]:08x})")
+    # Calculate base address for reply packet
+    next_rep_base_addr = 4 + word_per_page * next_req_page
+
+    # print(f"Reading reply size at addr {next_rep_base_addr:08x} (page {next_req_page})")
+    # Read the reply size register
+    repl_size_reg = mem.read(4 * next_rep_base_addr, 1)
+    repl_size = (repl_size_reg[0] & ~IPB_MB_OFFSET) + 1
+
+    # print(f"Reply size {repl_size} (reg {repl_size_reg[0]:08x})")
+    # Read the reply
     repl = mem.read(4*(next_rep_base_addr+1), repl_size)
 
-    b=b''
-    for v in repl:
-        print(f"{v:08x}")
-        b +=  v.to_bytes(4, byteorder='little', signed=False)
+    # Convert it into bytes
+    repl_bytes=b''
+    for val in repl:
+        # print(f"{v:08x}")
+        repl_bytes +=  val.to_bytes(4, byteorder='little', signed=False)
 
 
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    reply_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    #Set a timeout value of 1 second
-    clientSocket.settimeout(1)
+    # Set a timeout value of 1 second
+    reply_socket.settimeout(1)
 
-    # #Ping to server
-    # message = 'test'
-
-    # addr = ("127.0.0.1", 12000)
-    print(b)
-
-    clientSocket.sendto(b, addr)
+    # fire away
+    reply_socket.sendto(repl_bytes, addr)
 
 
---
+++ zynq/src/UDPSocket.hpp
@@ -0,0 +1,285 @@
+#pragma once
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#ifndef INPORT_ANY
+#define INPORT_ANY 0
+#endif
+
+#include <array>
+#include <cstring>
+#include <string>
+#include <vector>
+
+class UDPsocket
+{
+public:
+  typedef struct sockaddr_in sockaddr_in_t;
+  typedef struct sockaddr sockaddr_t;
+  typedef std::vector<uint8_t> msg_t;
+
+public:
+  struct IPv4;
+
+  enum class Status : int
+  {
+    OK = 0,
+    SocketError = -1,
+    OpenError = SocketError,
+    CloseError = -2,
+    ShutdownError = -3,
+    BindError = -4,
+    ConnectError = BindError,
+    SetSockOptError = -5,
+    GetSockNameError = -6,
+    SendError = -7,
+    RecvError = -8,
+    // AddressError = -66,
+  };
+
+private:
+  int sock{ -1 };
+  sockaddr_in_t self_addr{};
+  socklen_t self_addr_len = sizeof(self_addr);
+  sockaddr_in_t peer_addr{};
+  socklen_t peer_addr_len = sizeof(peer_addr);
+
+public:
+  UDPsocket()
+  {
+    self_addr = IPv4{};
+    peer_addr = IPv4{};
+  }
+
+  ~UDPsocket() { this->close(); }
+
+public:
+  int open()
+  {
+    this->close();
+    sock = (int)::socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (this->is_closed()) {
+      return (int)Status::SocketError;
+    }
+    return (int)Status::OK;
+  }
+
+  int close()
+  {
+    if (!this->is_closed()) {
+      int ret = ::shutdown(sock, SHUT_RDWR);
+      if (ret < 0) {
+        return (int)Status::ShutdownError;
+      }
+      ret = ::close(sock);
+      if (ret < 0) {
+        return (int)Status::CloseError;
+      }
+      sock = -1;
+    }
+    return (int)Status::OK;
+  }
+
+  bool is_closed() const { return sock < 0; }
+
+public:
+  int bind(const IPv4& ipaddr)
+  {
+    self_addr = ipaddr;
+    self_addr_len = sizeof(self_addr);
+    int opt = 1;
+    int ret = ::setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (const char*)&opt, sizeof(opt));
+    if (ret < 0) {
+      return (int)Status::SetSockOptError;
+    }
+    ret = ::bind(sock, (sockaddr_t*)&self_addr, self_addr_len);
+    if (ret < 0) {
+      return (int)Status::BindError;
+    }
+    ret = ::getsockname(sock, (sockaddr_t*)&self_addr, &self_addr_len);
+    if (ret < 0) {
+      return (int)Status::GetSockNameError;
+    }
+    return (int)Status::OK;
+  }
+
+  int bind(uint16_t portno)
+  {
+    auto ipaddr = IPv4::Any(portno);
+    return this->bind(ipaddr);
+  }
+
+  int bind_any() { return this->bind(INPORT_ANY); }
+
+  int bind_any(uint16_t& portno)
+  {
+    int ret = this->bind(INPORT_ANY);
+    if (ret < 0) {
+      return ret;
+    }
+    portno = IPv4{ self_addr }.port;
+    return (int)Status::OK;
+  }
+
+public:
+  int connect(const IPv4& ipaddr)
+  {
+    peer_addr = ipaddr;
+    peer_addr_len = sizeof(peer_addr);
+    int ret = ::connect(sock, (sockaddr_t*)&peer_addr, peer_addr_len);
+    if (ret < 0) {
+      return (int)Status::ConnectError;
+    }
+    return (int)Status::OK;
+  }
+
+  int connect(uint16_t portno)
+  {
+    auto ipaddr = IPv4::Loopback(portno);
+    return this->connect(ipaddr);
+  }
+
+public:
+  IPv4 get_self_ip() const { return self_addr; }
+
+  IPv4 get_peer_ip() const { return peer_addr; }
+
+public:
+  template<typename T, typename = typename std::enable_if<sizeof(typename T::value_type) == sizeof(uint8_t)>::type>
+  int send(const T& message, const IPv4& ipaddr) const
+  {
+    // // UPnP
+    // std::string msg = "M-SEARCH * HTTP/1.1\r\nHOST: 239.255.255.250:1900\r\nMAN: ssockp:discover\r\nST: ssockp:all\r\nMX: 1\r\n\r\n";
+    sockaddr_in_t addr_in = ipaddr;
+    socklen_t addr_in_len = sizeof(addr_in);
+    int ret = ::sendto(sock, (const char*)message.data(), message.size(), 0, (sockaddr_t*)&addr_in, addr_in_len);
+    if (ret < 0) {
+      return (int)Status::SendError;
+    }
+    return ret;
+  }
+
+  template<typename T, typename = typename std::enable_if<sizeof(typename T::value_type) == sizeof(uint8_t)>::type>
+  int recv(T& message, IPv4& ipaddr) const
+  {
+    sockaddr_in_t addr_in;
+    socklen_t addr_in_len = sizeof(addr_in);
+    typename T::value_type buffer[10 * 1024];
+    int ret = ::recvfrom(sock, (char*)buffer, sizeof(buffer), 0, (sockaddr_t*)&addr_in, &addr_in_len);
+    if (ret < 0) {
+      return (int)Status::RecvError;
+    }
+    ipaddr = addr_in;
+    message = { std::begin(buffer), std::begin(buffer) + ret };
+    return ret;
+  }
+
+public:
+  int broadcast(int opt) const
+  {
+    int ret = ::setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (const char*)&opt, sizeof(opt));
+    if (ret < 0) {
+      return (int)Status::SetSockOptError;
+    }
+    return (int)Status::OK;
+  }
+
+  int interrupt() const
+  {
+    uint16_t portno = IPv4{ self_addr }.port;
+    auto ipaddr = IPv4::Loopback(portno);
+    return this->send(msg_t{}, ipaddr);
+  }
+
+public:
+  struct IPv4
+  {
+    std::array<uint8_t, 4> octets{};
+    uint16_t port{};
+
+  public:
+    IPv4() {}
+
+    IPv4(const std::string& ipaddr, uint16_t portno)
+    {
+      int ret = ::inet_pton(AF_INET, ipaddr.c_str(), (uint32_t*)octets.data());
+      if (ret > 0) {
+        port = portno;
+      } else {
+        // throw std::runtime_error(Status::AddressError)
+      }
+    }
+
+    IPv4(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint16_t portno)
+    {
+      octets[0] = a;
+      octets[1] = b;
+      octets[2] = c;
+      octets[3] = d;
+      port = portno;
+    }
+
+    IPv4(const sockaddr_in_t& addr_in)
+    {
+      *(uint32_t*)octets.data() = addr_in.sin_addr.s_addr;
+      port = ntohs(addr_in.sin_port);
+    }
+
+    operator sockaddr_in_t() const
+    {
+      sockaddr_in_t addr_in;
+      std::memset(&addr_in, 0, sizeof(addr_in));
+      addr_in.sin_family = AF_INET;
+      addr_in.sin_addr.s_addr = *(uint32_t*)octets.data();
+      addr_in.sin_port = htons(port);
+      return addr_in;
+    }
+
+  private:
+    IPv4(uint32_t ipaddr, uint16_t portno)
+    {
+      *(uint32_t*)octets.data() = htonl(ipaddr);
+      port = portno;
+    }
+
+  public:
+    static IPv4 Any(uint16_t portno) { return IPv4{ INADDR_ANY, portno }; }
+    static IPv4 Loopback(uint16_t portno) { return IPv4{ INADDR_LOOPBACK, portno }; }
+    static IPv4 Broadcast(uint16_t portno) { return IPv4{ INADDR_BROADCAST, portno }; }
+
+  public:
+    const uint8_t& operator[](size_t octet) const { return octets[octet]; }
+    uint8_t& operator[](size_t octet) { return octets[octet]; }
+
+  public:
+    bool operator==(const IPv4& other) const { return this->octets == other.octets && this->port == other.port; }
+
+    bool operator!=(const IPv4& other) const { return !(*this == other); }
+
+  public:
+    std::string addr_string() const { return std::to_string(octets[0]) + '.' + std::to_string(octets[1]) + '.' + std::to_string(octets[2]) + '.' + std::to_string(octets[3]); }
+
+    std::string port_string() const { return std::to_string(port); }
+
+    std::string to_string() const { return this->addr_string() + ':' + this->port_string(); }
+
+    operator std::string() const { return this->to_string(); }
+  };
+};
+
+namespace std {
+template<>
+struct hash<UDPsocket::IPv4>
+{
+  typedef UDPsocket::IPv4 argument_type;
+  typedef size_t result_type;
+  result_type operator()(argument_type const& ipaddr) const noexcept
+  {
+    result_type const h1{ std::hash<uint32_t>{}(*(uint32_t*)ipaddr.octets.data()) };
+    result_type const h2{ std::hash<uint16_t>{}(ipaddr.port) };
+    return h1 ^ (h2 << 1);
+  }
+};
+}
