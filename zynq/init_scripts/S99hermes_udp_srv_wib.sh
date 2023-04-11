4d6490e97059323f3c2040a9bed685d8acb57789
Alessandro Thea <alessandro.thea@cern.ch>
2023-04-11 11:06:44 +0000

    Updating service parameters

--- zynq/init_scripts/S99hermes_udp_srv_wib.sh
+++ zynq/init_scripts/S99hermes_udp_srv_wib.sh
@@ -1,2 +1,2 @@
 echo "Starting Hermes IPBus UDP server"
-/bin/hermes_udp_srv wib 2>/var/log/hermes_udp_srv.err >/var/log/hermes_udp_srv.log &
+/bin/hermes_udp_srv -d wib -c false 2>/var/log/hermes_udp_srv.err >/var/log/hermes_udp_srv.log &
